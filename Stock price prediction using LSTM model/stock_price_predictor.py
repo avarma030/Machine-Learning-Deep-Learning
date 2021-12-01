e403a52c4197c45c2bebbddee5f8cf6559225c05
Akshay Varma <61402724+avarma030@users.noreply.github.com>
2021-12-01 17:37:43 +0100

    Updated code

--- Stock price prediction using LSTM model/stock_price_predictor.py
+++ Stock price prediction using LSTM model/stock_price_predictor.py
@@ -1,15 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+@author: avarma030
+"""
+
+# import libraries
+
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
+
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize']=20,10
 
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler(feature_range=(0,1))
 
+from keras.models import Sequential
+from keras.layers import LSTM,Dropout,Dense
+
+
+# read data
+
 df=pd.read_csv("SRTRANSFIN.NS.csv")
 df.head()
+
+
+# analyze the closing prices
 
 df["Date"]=pd.to_datetime(df.Date,format="%Y-%m-%d")
 df.index=df['Date']
@@ -17,24 +33,26 @@ df.index=df['Date']
 plt.figure(figsize=(16,8))
 plt.plot(df["Close"],label='Close Price history')
 
-from keras.models import Sequential
-from keras.layers import LSTM,Dropout,Dense
+
+# sort & filter 'date', 'close columns
 
 data=df.sort_index(ascending=True,axis=0)
-new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+filtered_data=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
 
 for i in range(0,len(data)):
-    new_dataset["Date"][i]=data['Date'][i]
-    new_dataset["Close"][i]=data["Close"][i]
+    filtered_data["Date"][i]=data['Date'][i]
+    filtered_data["Close"][i]=data["Close"][i]
     
+    
+# Normalization of filtered dataset
 
-new_dataset.index=new_dataset.Date
-new_dataset.drop("Date",axis=1,inplace=True)
+filtered_data.index=filtered_data.Date
+filtered_data.drop("Date",axis=1,inplace=True)
 
-final_dataset=new_dataset.values
+final_dataset=filtered_data.values
 
 train_data=final_dataset[0:987,:]
-valid_data=final_dataset[987:,:]
+validation_data=final_dataset[987:,:]
 
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(final_dataset)
@@ -49,21 +67,23 @@ x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
 
 x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
 
+
+# build & train the LSTM model
+
 lstm_model=Sequential()
 lstm_model.add(LSTM(units=50,return_sequences=True,input_shape=(x_train_data.shape[1],1)))
 lstm_model.add(LSTM(units=50))
 lstm_model.add(Dense(1))
 
-
-
-
 lstm_model.compile(loss='mean_squared_error',optimizer='adam')
 lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
 
-inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+inputs_data=filtered_data[len(filtered_data)-len(validation_data)-60:].values
 inputs_data=inputs_data.reshape(-1,1)
 inputs_data=scaler.transform(inputs_data)
 
+
+# take a test smaple
 
 X_test=[]
 for i in range(60,inputs_data.shape[0]):
@@ -74,10 +94,16 @@ X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 closing_price=lstm_model.predict(X_test)
 closing_price=scaler.inverse_transform(closing_price)
 
+
+# save the model
+
 lstm_model.save("saved_lstm_model.h5")
 
-train_data=new_dataset[:987]
-valid_data=new_dataset[987:]
-valid_data['Predictions']=closing_price
+
+# plot predicted cost with actual cost
+
+train_data=filtered_data[:987]
+validation_data=filtered_data[987:]
+validation_data['Predictions']=closing_price
 plt.plot(train_data["Close"])
-plt.plot(valid_data[['Close',"Predictions"]])
+plt.plot(validation_data[['Close',"Predictions"]])
